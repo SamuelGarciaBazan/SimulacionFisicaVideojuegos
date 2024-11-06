b773fd0eec2308928c26fccf9252b42d01256d5b
SamuelGarciaBazan <samuga05@ucm.es>
2024-11-06 09:34:31 +0100

    wind in progress

--- skeleton/WindForceGenerator.cpp
+++ skeleton/WindForceGenerator.cpp
@@ -17,3 +17,13 @@ physx::PxVec3 WindForceGenerator::forceCalculation(Particle* target)
 	return (k1 * relativeVel) + (k2 *(relativeVel.magnitude() * relativeVel));
 }
 
+//return true if target is in the area of the wind
+bool WindForceGenerator::afectCondition(Particle* target)
+{
+	auto pos = target->getPos();
+
+	return (pos.x > minRange.x && pos.x < maxRange.x &&
+			pos.y > minRange.y && pos.y < maxRange.y &&
+			pos.z > minRange.z && pos.z < maxRange.z);
+}
+
--- skeleton/WindForceGenerator.h
+++ skeleton/WindForceGenerator.h
@@ -10,8 +10,6 @@ public:
 
     virtual ~WindForceGenerator();
 
-
-
     const physx::PxVec3& getVelocity() const noexcept {
         return velocity;
     }
@@ -33,11 +31,29 @@ public:
         k2 = newK2;
     }
 
+    const physx::PxVec3& getMinRange() const noexcept {
+        return minRange;
+    }
+    void setMinRange(const physx::PxVec3& point) {
+        minRange = point;
+    }
+    const physx::PxVec3& getMaxRange() const noexcept {
+        return maxRange;
+    }
+    void setMaxRange(const physx::PxVec3& point) {
+        maxRange = point;
+    }
+
 protected:
 
     virtual physx::PxVec3 forceCalculation(Particle* target);
+    virtual bool afectCondition(Particle* target);
 
-    physx::PxVec3 velocity;
+
+    physx::PxVec3 velocity { 0,0,0 };
+
+    physx::PxVec3 minRange { 0,0,0 };
+    physx::PxVec3 maxRange { 0,0,0 };
 
     double k1 = 0.7;//coeficiente de rozamiento del aire + forma del objeto
     double k2 = 0;//coeficiente para ajustar la turbulencia a altas velocidades
